237dab796a4cde056a2c50f121b98f44525650a2
Fares ATOUI <faress.atoui@gmail.com>
2019-02-20 17:36:31 +0100

    filter fileName

--- src/fr/unice/miage/Main.java
+++ src/fr/unice/miage/Main.java
@@ -1,5 +1,6 @@
 package fr.unice.miage;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.lang.String;
 
 
@@ -23,10 +24,46 @@ import java.lang.String;
 
         }
 
+       private void filter(String path, String ext) {
+           File f = new File(path);
+
+           FilenameFilter textFilter = new FilenameFilter() {
+
+               public boolean accept(File dir, String name) {
+
+                   String lowercaseName = name.toLowerCase();
+                   if (lowercaseName.endsWith(ext) ) {
+                       return true;
+                   } else{
+                       return false;
+                   }
+               }
+           };
+
+           if (f.isDirectory()) {
+               for (File files: f.listFiles()) {
+
+                  filter(files.getPath(), ext);
+               }
+           } else {
+
+               if (textFilter.accept(f, f.getName())) {
+                   System.out.println("[ " + f.getPath() + " ]");
+               }
+           }
+       }
+
+
         public static void main(String[] args) {
             Main m = new Main();
+            System.out.println(" ");
 
-            m.print_directory_contents(".");
+
+          //  m.print_directory_contents(".");
+
+            m.filter(".", ".java");
+
+
         }
 
     }
